04fba4d0e6f012efda726bd4180d2a2eef2ca5d2
Alexey <teos.nl@gmail.com>
2017-03-13 18:05:05 +0300

    2 secs

---
+++ build/scripts/app.js
@@ -0,0 +1,31 @@
+$(function () {
+
+    $("#mainFullpage").fullpage({
+        afterRender: function () {
+
+            $('.sec1__auto').viewportChecker({
+                classToAdd: '',
+                classToAddForFullView: 'sec1__auto_show'
+            });
+
+            $('.sec2__img_auto').viewportChecker({
+                classToAdd: 'sec2__img_move',
+                classToAddForFullView: 'sec2__img_move'
+            });
+        },
+
+        onLeave: function (index, nextIndex, direction) {
+
+            if (nextIndex == 2) {
+                $('.sec2__img_auto').addClass("sec2__img_move");
+            }
+            console.log(index);
+        }
+    });
+
+    var demo = new CountUp("sec2__number", 0, 2000, 0, 2.5, {
+        separator: ""
+    });
+    demo.start();
+});
+//# sourceMappingURL=app.js.map
